e2c5bca05759232e86644ba9e0d310647877316a
geekyogeshcode <officialyogesh7218@gmail.com>
2024-10-22 18:54:41 +0530

    Label Addel

--- React/Form/src/App.jsx
+++ React/Form/src/App.jsx
@@ -14,23 +14,27 @@ function App() {
      <div className="App">
       <h1>Login Form</h1>
       <form action="" onSubmit={handleSubmit(onSubmitForm )}>
-            <input type="text" placeholder='userName'  {...register('username',
+           <label htmlFor="user" style={{textAlign:"left"}}>UserName</label>
+            <input type="text" id='user' placeholder='userName'  {...register('username',
               {required:"Username is Required",
                 maxLength:{value:20,message:"username must be less than 20 characters"},
                 minLength:{value:5,message:"Input must be greater than 5 characters"}
               })} />
             {errors.username && <p>{errors.username.message}</p>}
 
+            <label htmlFor="user" style={{textAlign:"left"}}>Email</label>
             <input type="email" placeholder='Email'   {...register('email',
             {required:"Email is Required",
             })} />
             {errors.email && <p>{errors.email.message}</p>}
 
+            <label htmlFor="user" style={{textAlign:"left"}}>Number</label>
             <input type="number" placeholder='Number'    {...register('number',{
               required:"Number is Required"
             })} />
             {errors.number && <p>{errors.number.message}</p>}
 
+            <label htmlFor="user" style={{textAlign:"left"}}>Password</label>
             <input type="password" placeholder='Password'    {...register('password',{
               required:"Password is Required",minLength:{
                 value:10,
